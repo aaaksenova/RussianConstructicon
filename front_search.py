d2c1294ae6f6adde2f4aa1dbd1b0f31ddadb3c76
19336136 <aksanna_a@mail.ru>
2021-10-31 11:14:13 +0300

    fixed nltk

--- front_search.py
+++ front_search.py
@@ -5,6 +5,12 @@ import pymorphy2
 import nltk
 import streamlit as st
 import time
+import os
+
+root = os.path.dirname(os.path.abspath(__file__))
+download_dir = os.path.join(root, 'nltk_data')
+os.chdir(download_dir)
+nltk.data.path.append(download_dir)
 # nltk.download('punkt')
 
 morph = pymorphy2.MorphAnalyzer()
